2120211149dfae985dbd758ae2e5913de856fd68
Ashutosh Salunkhe <YOUR_GITHUB_NOREPLY_EMAIL>
2025-10-02 22:56:25 +0530

    Fix Express wildcard route in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ app.use("/api/checkProducts" , checkProducts)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
   });
 }
